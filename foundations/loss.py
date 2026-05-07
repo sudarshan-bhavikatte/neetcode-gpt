fe935044e5c36220d735d44750ff5db5ae637b89
sudarshan-bhavikatte <149920240+sudarshan-bhavikatte@users.noreply.github.com>
2026-05-07 15:52:06 +0530

    Implement Cross-entropy loss (Math Foundations 4/6)

---
+++ foundations/loss.py
@@ -0,0 +1,34 @@
+import numpy as np
+from numpy.typing import NDArray
+
+
+class Solution:
+
+    def binary_cross_entropy(self, y_true: NDArray[np.float64], y_pred: NDArray[np.float64]) -> float:
+        # y_true: true labels (0 or 1)
+        # y_pred: predicted probabilities
+        # Hint: add a small epsilon (1e-7) to y_pred to avoid log(0)
+        # return round(your_answer, 4)
+        epsilon = 1e-7
+        y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
+        return -1 * np.round(
+            (np.mean(
+                (y_true * np.log(y_pred)) + ((1 - y_true) * np.log((1 - y_pred))) 
+            )), 4
+        )
+        
+
+    def categorical_cross_entropy(self, y_true: NDArray[np.float64], y_pred: NDArray[np.float64]) -> float:
+        # y_true: one-hot encoded true labels (shape: n_samples x n_classes)
+        # y_pred: predicted probabilities (shape: n_samples x n_classes)
+        # Hint: add a small epsilon (1e-7) to y_pred to avoid log(0)
+        # return round(your_answer, 4)
+        y_pred = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        return -np.round(
+            np.mean(
+                np.sum(
+                    y_true * np.log(y_pred), axis = 1
+                )
+            ),4
+        )
